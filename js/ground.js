ff127fb4ba89f8aeeec76814b5eedbb1f478e60a
Kushalkhemka <Kushalkhemka559@gmail.com>
2020-11-06 22:28:52 +0530

    modified

--- js/ground.js
+++ js/ground.js
@@ -11,7 +11,7 @@ class Ground{
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("white");
+      fill(txtcolor);
     rect(pos.x, pos.y, this.w,this.h);
     }
   };
